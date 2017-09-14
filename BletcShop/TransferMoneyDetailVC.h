fe0318bc4878616b440c44f44f3af9f05035386c
apple <apple@tang.local>
2017-09-14 14:59:51 +0800

    jhchg

---
+++ BletcShop/TransferMoneyDetailVC.h
@@ -0,0 +1,13 @@
+//
+//  TransferMoneyDetailVC.h
+//  BletcShop
+//
+//  Created by apple on 2017/9/14.
+//  Copyright © 2017年 bletc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TransferMoneyDetailVC : UIViewController
+@property (nonatomic , strong) UITableView *tableview;// <#Description#>
+@end
